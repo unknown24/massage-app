bff7f391c61e7abb0a92bb5fae1adbcabe21d29d
aep <aep.stmik@gmail.com>
2019-11-20 03:16:48 +0700

    Fix error

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,6 @@
-// babel.config.js
-module.exports = {
-  plugins: [
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-transform-regenerator',
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        helpers: false,
-        regenerator: true,
-      },
-    ],
-  ],
-  presets: [
-    '@babel/preset-env',
-    'module:metro-react-native-babel-preset',
-    '@babel/preset-react',
-    '@babel/typescript',
-  ],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
